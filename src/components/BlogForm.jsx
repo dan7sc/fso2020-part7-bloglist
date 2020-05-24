a3c901a09666d095b58231705eba255aa3029945
dan7sc <su7cheng@gmail.com>
2020-05-24 14:14:25 -0300

    Refactor BlogForm component

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
+import { setNotification } from '../reducers/notificationReducer'
+import { initializeBlogs, createBlog } from '../reducers/blogReducer'
 
-const BlogForm = ({ createBlog }) => {
+const BlogForm = () => {
+  const dispatch = useDispatch()
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [url, setUrl] = useState('')
@@ -10,18 +13,41 @@ const BlogForm = ({ createBlog }) => {
     setFunction(event.target.value)
   }
 
-  const addBlog = (event) => {
+  const addBlog = async (event) => {
     event.preventDefault()
 
-    createBlog({
+    const newObject = {
       title,
       author,
       url
-    })
+    }
 
     setTitle('')
     setAuthor('')
     setUrl('')
+
+    try {
+      await dispatch(
+        createBlog(newObject)
+      )
+      dispatch(
+        initializeBlogs()
+      )
+
+      dispatch(
+        setNotification([
+          'success',
+          `a new blog ${newObject.title} by ${newObject.author} added`
+        ])
+      )
+    } catch(e)  {
+      dispatch(
+        setNotification([
+          'error',
+          'fail to add a new blog'
+        ])
+      )
+    }
   }
 
   return (
@@ -64,10 +90,6 @@ const BlogForm = ({ createBlog }) => {
       </form>
     </div>
   )
-}
-
-BlogForm.propTypes = {
-  createBlog: PropTypes.func.isRequired
 }
 
 export default BlogForm
